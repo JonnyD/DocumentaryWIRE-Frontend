1dac627d5aada1cd862793233c1645983c2f6787
Jonathan Devine <dev@jonnydevine.com>
2019-07-05 18:47:37 +0100

    Created DocumentaryService

--- src/app/services/documentary.service.ts
+++ src/app/services/documentary.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DocumentaryService {
+export class DocumentaryService extends DataService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super('http://localhost:8000/api/documentaries', http);
+   }
 }
