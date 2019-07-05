2af9faa435e433a95027698e9a44fd92095d601a
Jonathan Devine <dev@jonnydevine.com>
2019-07-05 18:42:23 +0100

    Added CRUD methods

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  constructor(private url: string, private http: HttpClient) { }
 
-  constructor() { }
+  getAll() {
+    return this.http.get<Object[]>(this.url);
+  }
+
+  create(resource) {
+    return this.http.post(this.url, JSON.stringify(resource));
+  }
+
+  update(resource) {
+    return this.http.put(this.url + '/' + resource.id, JSON.stringify({
+      
+    })));
+  }
+
+  delete(id: number) {
+    return this.http.delete(this.url + '/' + id);
+  }
 }
