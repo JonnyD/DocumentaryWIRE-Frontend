c07de433d35323f782756f68bd1f9207df2f802e
Jonathan Devine <dev@jonnydevine.com>
2019-11-26 23:04:18 +0000

    Added loading text

--- src/app/public/register/confirm/confirm.component.ts
+++ src/app/public/register/confirm/confirm.component.ts
@@ -51,6 +51,8 @@ export class ConfirmComponent implements OnInit {
                 "class": "success"
               }
             ];
+
+            this.loading = false;
           },
           error => {
             console.log(error);
@@ -60,6 +62,8 @@ export class ConfirmComponent implements OnInit {
                 "class": "danger"
               }
             ];
+
+            this.loading = false;
           }
           );
     });
