55c67ebe21dab835d418eff27bbbccdb16a5f997
Jonathan Devine <dev@jonnydevine.com>
2020-04-21 23:29:28 +0100

    Chat placeholder

---
+++ src/app/public/chat/chat.component.ts
@@ -0,0 +1,14 @@
+import { AuthenticationService } from '../services/authentication.service';
+import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+  
+    ngOnInit() {
+    }
+}
